6150f3c6a541414e931142f81713c0d18abb1ac1
Cristinel-Gabriel ILIE (88139) <cristinel.ilie@stud.etti.upb.ro>
2023-12-05 16:48:59 +0200

    Tema week 2

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#declarare lista
+lista = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 
+# lista ordonata ascendent
+print(sorted(lista))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#lista ordonata descendent
+print(sorted(lista,reverse=True))
 
+#numere cu indici pari
+print(lista[::2])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#numere cu indici impari
+print(lista[1::2])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#elemente multipli de 3
+print(lista[::3])
